ad277a823f56de1e10c0d433c3e6670edeb42f41
madhupriyamurali <50649243+madhupriyamurali@users.noreply.github.com>
2019-05-15 18:12:18 +0530

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,6 +3,6 @@ public class HelloWorld {
 
 	public static void main(String args[]){
 		System.out.println("Welcome to Sample Project");
-		System.out.println("Test commit from git repo");
+		System.out.println("Testing java commit from git repo");
 	}
 }
